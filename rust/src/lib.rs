618a60cad7791aeb2bffb644051a58d7ea1f122e
Totobird <45902300+Totobird-Creations@users.noreply.github.com>
2025-03-19 13:36:40 -0400

    Prototype friendly user-facing api for Rust;

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,51 +1,22 @@
-unsafe extern "C" {
-    pub fn _vx_sleep(secs : u64, subset_nanos : u32) -> ();
-    pub fn _vx_plot_id() -> u64;
-    pub fn _vx_plot_name_fmt(out_ptr : u32) -> ();
-    pub fn _vx_player_send_message(session_id : u64, msg_ptr : u32, msg_size : u32) -> ();
-    pub fn _vx_player_send_sound(session_id : u64, sound_id_ptr : u32, sound_id_size : u32, cat : u32, volume : f32, pitch : f32, seed : u64) -> ();
-}
+mod lhmc;
+use lhmc::*;
+use core::time::Duration;
+
 
 #[unsafe(no_mangle)]
 pub fn _vx_on_player_join(session_id : u64) {
-    let plot_id = unsafe { _vx_plot_id() };
+    let player    = Player::from_session_id(session_id);
+    let plot_id   = Plot::id();
+    let plot_name = Plot::name_fmt();
 
-    let mut plot_name = (0u32, 0u32,);
-    unsafe { _vx_plot_name_fmt(&mut plot_name as *mut _ as u32) };
-    
-    let plot_name = unsafe { String::from_raw_parts(plot_name.0 as *mut u8, plot_name.1 as usize, plot_name.1 as usize) };
-
-    let msg = format!("<obf><black>###</></obf> <green>Welcome to</> <gold>plot</> <red><u>{}</></><pink><b>!</></>", plot_id);
-    unsafe { _vx_player_send_message(session_id, msg.as_ptr() as u32, msg.as_bytes().len() as u32); }
-
-    let msg = format!("Plot Name: \"{}\"", plot_name.replace("\\", "\\\\").replace("<", "\\<").replace("\"", "\\\\\""));
-    unsafe { _vx_player_send_message(session_id, msg.as_ptr() as u32, msg.as_bytes().len() as u32); }
+    player.send_chat(&format!("<obf><black>###</></obf> <green>Welcome to</> <gold>plot</> <red><u>{}</></><pink><b>!</></>", plot_id));
+    player.send_chat(&format!("Plot Name: {}", sanitise_text(&format!("{:?}", plot_name))));
 
     for i in 0..3 {
-        unsafe { _vx_sleep(0, 250000000); }
-
-        let msg = format!("<b><#ff0000>{}</></>", i);
-        unsafe { _vx_player_send_message(session_id, msg.as_ptr() as u32, msg.as_bytes().len() as u32); }
+        sleep(Duration::from_millis(500));
+        player.send_chat(&format!("<b><#ff0000>{}</></>", i));
     }
-    unsafe { _vx_sleep(0, 250000000); }
+    sleep(Duration::from_millis(500));
 
-    let id = "minecraft:entity.player.levelup";
-    unsafe { _vx_player_send_sound(session_id, id.as_ptr() as u32, id.as_bytes().len() as u32, 0, 1.0, 2.0, 0); }
-}
-
-
-#[unsafe(no_mangle)]
-pub unsafe fn _vx_alloc(size : u32, align : u32) -> u32 {
-    use std::alloc::*;
-    let layout = unsafe { Layout::from_size_align_unchecked(size as usize, align as usize) };
-    let ptr = unsafe { alloc(layout) };
-    if (ptr.is_null()) { handle_alloc_error(layout); }
-    ptr as u32
-}
-
-#[unsafe(no_mangle)]
-pub unsafe fn _vx_dealloc(ptr : u32, size : u32, align : u32) -> () {
-    use std::alloc::*;
-    let layout = unsafe { Layout::from_size_align_unchecked(size as usize, align as usize) };
-    unsafe { dealloc(ptr as *mut u8, layout) };
+    player.play_sound("entity.player.levelup", SoundCat::Player, 1.0, 2.0, 0);
 }
